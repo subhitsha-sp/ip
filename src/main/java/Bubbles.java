7bf12acb83f9c0dd73fb1e2ea982763d0afd4a81
Subhitsha Prabakaran <subhitsha.sp@gmail.com>
2025-08-19 22:09:37 +0800

    Mark as Done

--- src/main/java/Bubbles.java
+++ src/main/java/Bubbles.java
@@ -5,7 +5,7 @@ public class Bubbles {
     public static void main(String[] args) {
         Message intro = new Message("Hello! I'm Bubbles 🫧 \nWhat can I do for you?");
         Message exit = new Message("Byeee 👋");
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Task> list = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         String line = "--------------------------------------------------------";
@@ -15,23 +15,43 @@ public class Bubbles {
         String command = scanner.nextLine();
         System.out.println(line);
         if(!command.equals("list")){
-            list.add(command);
+            list.add(new Task(command));
         }
         while (!command.equals("bye")) {
-            if(!command.equals("list")) {
+            if(!(command.equals("list") || command.startsWith("mark") || command.startsWith("unmark"))) {
                 System.out.println("\tadded: " + command);
             }
             System.out.println(line);
             command = scanner.nextLine();
             System.out.println(line);
 
-            if(!command.equals("list")){
-                list.add(command);
+            if(!command.equals("list") && !command.contains("mark")){
+                list.add(new Task(command));
             }
-            else{
+            if(command.equals("list")){
+                System.out.println("Here are the tasks in your list: ");
                 for(int i = 0; i < list.size(); i++){
-                    System.out.println("\t" + (i+1) + ". " + list.get(i));
+                    Task task = list.get(i);
+                    System.out.println("\t" + (i+1) + ".[" + task.getStatusIcon() +"] " + task.toString());
                 }
+            }
+
+            if(command.startsWith("mark")){
+                String[] words = command.split(" ");
+                Task task = list.get(Integer.parseInt(words[1])-1);
+                task.setDone();
+
+                System.out.println("Yayyy! Let's pop this task off!");
+                System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
+            }
+
+            if(command.startsWith("unmark")){
+                String[] words = command.split(" ");
+                Task task = list.get(Integer.parseInt(words[1])-1);
+                task.setNotDone();
+
+                System.out.println("Okieee! Let's bubble it back in!");
+                System.out.println("[" + task.getStatusIcon() + "] " + task.getDescription());
             }
         }
 
